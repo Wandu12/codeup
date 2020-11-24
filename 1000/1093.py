6430911112e928f473bb4ab73069b3c7eaa0d2bd
wandu <58932926+wandudev@users.noreply.github.com>
2020-11-24 17:55:55 +0900

    Update 1093.py

--- 1000/1093.py
+++ 1000/1093.py
@@ -1,12 +1,13 @@
 # 1093.py
 a = int(input())
 stuCall = input().split()
-stuCnt = [0 for _ in range(22)]
+stuCnt = [0 for _ in range(24)]
 
 for i in range(0, a):
-    for j in range(0, 22):
+    for j in range(0, 24):
         if int(stuCall[i]) == j:
             stuCnt[j] += 1
 
-for j in range(0, 22):
+for j in range(1, 24):
     print(stuCnt[j], end=" ")
+print("\n")
