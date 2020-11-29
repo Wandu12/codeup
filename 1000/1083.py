448050b1976019d56b322368bdd36236d31fecdd
wandu <58932926+wandudev@users.noreply.github.com>
2020-11-29 23:40:28 +0900

    Update 1083.py

--- 1000/1083.py
+++ 1000/1083.py
@@ -1,8 +1,14 @@
-# 1083
-n = input()
-n1 = int(n)
-for i in range(1, n1+1):
-    if(i%3 == 0): 
+# 1083: 33은 XX로 나오게 수정
+n = int(input())
+for i in range(1, n+1):
+    a = int(i/10)
+    b = i%10
+    if(a == 3 or a == 6 or a == 9): 
+        if(b == 3 or b == 6 or b == 9):
+            print("XX", end=" ")
+            continue
         print("X", end=" ")
+    elif(b == 3 or b == 6 or b == 9):
+        print("X", end=" ")    
     else:
         print(i, end=" ")
