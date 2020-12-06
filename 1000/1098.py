81344eaf4ae33300e9885ef421b88711b7b5f640
wandu <58932926+wandudev@users.noreply.github.com>
2020-12-06 23:23:53 +0900

    edit 1098

--- 1000/1098.py
+++ 1000/1098.py
@@ -1,18 +1,23 @@
-# 1098
-h,w = map(int,input().split())
-n = int(input())
-array = [[0 for col in range(w)] for row in range(h)]
+# 1098 : 오류 수정(격자판 세로, 가로 입력. 가로, 세로 입력X)
+h, w = map(int, input().split()) # 격차판 세로, 가로 입력 h, w
+n = int(input()) # 놓을 막대의 개수 n
 
-for i in range(n):
-    l,d,x,y = map(int,input().split())
-    if d == 1 :
-        for q in range(l) :
-            array[x-1+q][y-1] = 1
-    if d == 0 :
-        for w in range(l) :
-            array[x-1][y+w-1] = 1
+# w*h 배열 만들고 0으로 초기화
+matrix = [[0 for col in range(w)] for row in range(h)] 
+    # [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    #일차원은 wList = [0 for i in range(w)]
 
-for z in array :
-    for k in z :
-        print(k, end=" ")
-    print(")
+for i in range(0, n):
+    l, d, x, y = map(int, input().split()) 
+        # 막대길이 l, 방향 d = 0이 가로 1이 세로, 좌표(x,y)
+    if d==1:
+        for j in range(l): #(0, l)에서 변경
+            matrix[x-1+j][y-1] = 1
+    if d==0:
+        for k in range(l):
+            matrix[x-1][y+k-1] = 1
+
+for m in matrix:        # a에서 안쪽 리스트를 꺼냄
+    for o in m:    # 안쪽 리스트에서 요소를 하나씩 꺼냄
+        print(o, end=' ')
+    print()
