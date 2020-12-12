b8eb385ead7284722b2a392277017566b1650795
wandu <58932926+wandudev@users.noreply.github.com>
2020-12-12 23:17:53 +0900

    add 4344

---
+++ baekjoon/vector7_4344.py
@@ -0,0 +1,12 @@
+# vector7_4344
+c = int(input())
+for i in range(c):
+    n = list(map(int, input().split())
+    avg = sum(n[1:]) / n[0]
+    cnt = 0
+
+    for j in n[1:]:
+        if j > avg : 
+            cnt += 1
+            
+    print(str("%.3f" %round((cnt/n[0])*100, 3))+"%")
