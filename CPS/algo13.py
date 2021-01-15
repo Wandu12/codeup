2ae2b569cecd2ad332c40275767d9fe2340c8c95
wandu <58932926+wandudev@users.noreply.github.com>
2021-01-15 23:59:03 +0900

    add 13

---
+++ CPS/algo13.py
@@ -0,0 +1,18 @@
+# algo13
+digit = 0
+res = 0
+j = 0
+ch = []
+a = []
+n = int(input())
+
+for j in range(0, a[j] != '\0'):
+    digit = a[j] - 48
+    ch[digit] += 1
+
+for i in range(0, 10):
+    if(ch[i] >= max):
+        max = ch[i]
+        res = i
+
+print(res)
