2035bc43a88ebfb9bc964e58ab7f943c3e159b0d
wandu <58932926+wandudev@users.noreply.github.com>
2021-01-26 23:41:01 +0900

    add 9

---
+++ CPS/algo09.py
@@ -0,0 +1,10 @@
+# algo09
+n = int(input())
+divisor = 0
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        if (i%j == 0):
+            divisor += 1
+    print(divisor, end=" ")
+    divisor = 0
